b288e137e9d8b4af126e07bfb74ce4e5f8c90e06
Jose Angel Orozco <31044334+LaughingDead@users.noreply.github.com>
2017-09-15 10:24:19 -0700

    Add files via upload

--- point_01.js
+++ point_01.js
@@ -1,14 +1,26 @@
-// Initial p5.js point project
+<!DOCTYPE html>
+<html>
 
-function setup() {
-    CreateCanvas (480, 120);
+<head>
+  <meta name="description" content="JoseAngel Orozco Branch">
+  <script data-require="p5.js@*" data-semver="0.5.7" src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.7/p5.min.js"></script>
+</head>
 
-}
+<body>
+  
+  <script>
+    
+    function setup() {
+      createCanvas(480, 120)
+    }
 
-function draw() {
-  background(200, 100,50);
+    function draw() {
+      background(20, 180, 210);
+      strokeWeight(2);
+      point( width/2, height/2 );
+      
+    }
+  </script>
+</body>
 
-  strokeWeight(2); 
-  point( width/2, height/2 );
-    ellipse(25, 25, 25, 25);
-}
+</html>
