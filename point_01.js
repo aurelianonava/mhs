698a585323c83f0d773dd9aafb2b5f14eff70b2c
Aureliano <sersinfin@gmail.com>
2017-09-19 13:08:39 -0700

    Modifed index.html and point_01.js.

--- point_01.js
+++ point_01.js
@@ -1,7 +1,9 @@
 // Initial p5.js point project
 
+var x = 10;
+
 function setup() {
-    createCanvas (480, 120);
+    createCanvas (480, 220);
 
 }
 
@@ -9,6 +11,6 @@ function draw() {
   background(20,200,150);
 
   strokeWeight(2); 
-  point( width/2, height/2 );
-    ellipse(25, 25, 25, 25);
+  point( x+=1, 10 );
+    ellipse(width/2, height/2, 25, 25);
 }
