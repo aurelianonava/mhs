5d4c2adb12f57cdf7bbb06f300f1c4b88d1f6fa4
John <31355664+Outrights@users.noreply.github.com>
2017-09-19 11:39:21 -0700

    Point_01.js updated to Project 2 starting point.

--- point_01.js
+++ point_01.js
@@ -1,14 +1,24 @@
 // Initial p5.js point project
 
+var x; //variable x declared
+var y; //variable y declared
+var speed= 5;
+
+x=10; // variable x intialize at 10
+y=60; // variable y intialized at 60
+
+
 function setup() {
-    createCanvas (480, 120);
+createCanvas (480, 120);
 
 }
 
 function draw() {
-  background(20,200,150);
+  background(200, 100,50);
 
-  strokeWeight(2); 
-  point( width/2, height/2 );
-    ellipse(25, 25, 25, 25);
+  strokeWeight(3); 
+  point(x+=1, 25);
+    if (x > 240) {
+    x =0;    }
+
 }
