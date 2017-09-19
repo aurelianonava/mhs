b5749246e8f931ce4e381dd0a030aab081971441
Aureliano <sersinfin@gmail.com>
2017-09-19 12:53:27 -0700

    Revert "Changed point()."
    This reverts commit 84d53b8b2ae5df6f28eb3bc570233d6fce58525c.

--- point_01.js
+++ point_01.js
@@ -1,9 +1,7 @@
 // Initial p5.js point project
 
-var x = 10, y = 10;
-
 function setup() {
-    createCanvas (480, 420);
+    createCanvas (480, 120);
 
 }
 
@@ -11,7 +9,6 @@ function draw() {
   background(20,200,150);
 
   strokeWeight(2); 
-  point(x+=1, y+=1);
-  ellipse(width/2, height/2, 25, 25);
-  
+  point( width/2, height/2 );
+    ellipse(25, 25, 25, 25);
 }
