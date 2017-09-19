84d53b8b2ae5df6f28eb3bc570233d6fce58525c
Aureliano <sersinfin@gmail.com>
2017-09-19 12:50:36 -0700

    Changed point().

--- point_01.js
+++ point_01.js
@@ -1,7 +1,9 @@
 // Initial p5.js point project
 
+var x = 10, y = 10;
+
 function setup() {
-    createCanvas (480, 120);
+    createCanvas (480, 420);
 
 }
 
@@ -9,6 +11,7 @@ function draw() {
   background(20,200,150);
 
   strokeWeight(2); 
-  point( width/2, height/2 );
-    ellipse(25, 25, 25, 25);
+  point(x+=1, y+=1);
+  ellipse(width/2, height/2, 25, 25);
+  
 }
