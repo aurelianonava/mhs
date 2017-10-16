df6f2856482255c57b8a4084fdaa15d29e9cc6b1
Aureliano <sersinfin@gmail.com>
2017-10-16 08:44:07 -0700

    Added six names to project.

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ function setup() {
 function draw() {
   background(150);
   
-  fill(150);
+  fill(125, 170, 220);
   rect(0,0,200,200);
+  fill(150);
   rect(200,0,200, 200);
   rect(400,0,200,200);
   rect(0,200,200,200);
@@ -26,29 +27,29 @@ function draw() {
   // bounce x
   x = x + xspeed;
 
-  if (x > width / 3 || x < 0) {
+  if (x > (width / 3)-12.5 || x < 12.5) {
     xspeed = -xspeed;
   }
 
   // bounce y
   y = y + yspeed;
 
-  if (y > height / 2 || y < 0) {
+  if (y > (height / 2)-12.5 || y < 12.5) {
     yspeed = -yspeed;
   }
 
   n = "Mr. Nava";
-  jdg = "Juan";
+  jdg = "Juan Guzman";
   jg = "John Gonzalez";
   jo = "Jose Orozco";
-  s5 = "Student #5";
-  s6 = "Student #6";
+  ag = "Angel Gomez";
+  me = "Michael Esparza";
   //
   fill(50);
   text(n, 10, 10, 70, 80); // Text wraps within text box
   text(jdg, 210, 10, 70, 80);
   text(jg, 410, 10, 100, 80);
   text(jo, 10, 210, 100, 80);
-  text(s5, 210, 210, 70, 80);
-  text(s6, 410, 210, 70, 80);
+  text(ag, 210, 210, 70, 80);
+  text(me, 410, 210, 70, 80);
 }
