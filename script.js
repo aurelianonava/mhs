241358f6cb45d3f1c447e1cc5f0650e49cd009a6
Aureliano <sersinfin@gmail.com>
2017-10-26 09:32:59 -0700

    Updated Monica Jaimes.

--- script.js
+++ script.js
@@ -165,18 +165,18 @@ function draw() {
   
   // text() function variables
   n = "Mr. Nava";
-  jdg = "Juan Guzman";
+  mj = "Monica Jaimes";
   jg = "John Gonzalez";
   jo = "Jose Orozco";
   ag = "Angel Gomez";
   me = "Michael Esparza";
   da = "Daniel Ayala";
-  mj = "Monica Jaimes";
+  ns = "New Student";
   
   // Text function for names
   fill(50);
   text(n, 10, 10, 70, 80); // Text wraps within text box
-  text(jdg, 210, 10, 70, 80);
+  text(mj, 210, 10, 70, 80);
   text(jg, 410, 10, 100, 80);
   text(jo, 10, 210, 100, 80);
   text(ag, 210, 210, 70, 80);
